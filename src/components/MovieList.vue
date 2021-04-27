57e8a1d06dfd7a353283ad9e6e791a50f2bff9ea
tur342 <syisdreaming@gmail.com>
2021-04-27 22:52:15 +0900

    upload files

---
+++ src/components/MovieList.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Movie List..</div>
+</template>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="title"
+            outlined>
+            <template v-slot:prepend-inner>
+                <v-icon>search</v-icon>
+            </template>
+        </v-text-field>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                title: ''
+            }
+        }
+    }
+</script>
